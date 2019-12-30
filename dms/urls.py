10150bc0824fa7d1df3f1047f60946fefa99ef9c
anthony <anthonyt.wallace@icloud.com>
2019-12-29 21:24:04 -0800

    started work on mockup version

--- dms/urls.py
+++ dms/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages import views
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('generic', views.generic, name='generic'),
-    path('elements', views.elements, name='elements'),
-    path('contact', views.contact, name='contact'),
-    path('', views.home, name='home')
+    path('', include('pages.urls'))
 ]
