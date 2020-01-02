e1e393f5ff10cdde3ac3dc983cdd4aa22faf8dcf
anthony <anthonyt.wallace@icloud.com>
2020-01-01 17:24:45 -0800

    added smooth scroll to navbar

---
+++ assets/js/smoothScroll.js
@@ -0,0 +1,36 @@
+smoothScroll = (selector, duration) => {
+    const startPos = window.pageYOffset
+    const target = document.querySelector(selector)
+    const targetPos = target.getBoundingClientRect().top
+    const dist = targetPos - startPos
+    let startTime = null
+
+
+    animation = (currentTime) => {
+        if (startTime === null) startTime = currentTime
+        let timeElapsed = currentTime - startTime
+        let animationFrame = ease(timeElapsed, startPos, dist, duration)
+        window.scrollTo(0, animationFrame)
+        if(timeElapsed < duration) requestAnimationFrame(animation)
+    }
+
+    // ease function stolen from interwebs :)
+    ease = (t, b, c, d) => {
+        t /= d /2
+        if (t < 1) return c / 2 * t * t + b
+        t--
+        return -c / 2 * (t * (t - 2) - 1) + b
+    }
+
+    requestAnimationFrame(animation)
+}
+
+const home = document.querySelector('#a-home')
+const about = document.querySelector('#a-about')
+const services = document.querySelector('#a-services')
+const work = document.querySelector('#a-work')
+
+home.addEventListener('click', () => smoothScroll('#home', 1000))
+about.addEventListener('click', () => smoothScroll('#about', 1000))
+services.addEventListener('click', () => smoothScroll('#services', 1000))
+work.addEventListener('click', () => smoothScroll('#work', 1000))
